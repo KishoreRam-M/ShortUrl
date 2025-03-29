876888f45d313780da6ddbba5799f8ad3ead08fa
KISHORE RAM M <mrkisho65@outlook.com>
2025-03-29 12:02:02 +0530

    Add files via upload

---
+++ Restart/UserDetails/UrlShortend/UrlShortend.Backend/src/main/java/com/UrlShortend/Backend/Model/ClickEvents.java
@@ -0,0 +1,19 @@
+package com.UrlShortend.Backend.Model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class ClickEvents {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime clickDate;
+    @ManyToOne
+    @JoinColumn(name = "Url_mapping_id")
+    private Url_Mapping UrlMapping;
+}
