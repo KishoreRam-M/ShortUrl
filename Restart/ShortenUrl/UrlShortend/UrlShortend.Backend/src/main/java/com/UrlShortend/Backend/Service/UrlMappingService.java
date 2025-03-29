916b849b46b5abf34e9369b10a448cfa9fc0e180
KISHORE RAM M <mrkisho65@outlook.com>
2025-03-29 16:51:39 +0530

    Add files via upload

---
+++ Restart/ShortenUrl/UrlShortend/UrlShortend.Backend/src/main/java/com/UrlShortend/Backend/Service/UrlMappingService.java
@@ -0,0 +1,53 @@
+package com.UrlShortend.Backend.Service;
+
+import com.UrlShortend.Backend.Dtos.UrlMappingDTO;
+import com.UrlShortend.Backend.Model.Url_Mapping;
+import com.UrlShortend.Backend.Model.Users;
+import com.UrlShortend.Backend.Repo.UrlMappingRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class UrlMappingService {
+    private final UrlMappingRepo repo;
+
+    public UrlMappingDTO createShortUrl(String originalUrl, Users user) {
+        String shortUrl = generateShortUrl();
+
+        Url_Mapping urlMapping = new Url_Mapping();
+        urlMapping.setOriginalUrl(originalUrl);
+        urlMapping.setUsers(user);
+        urlMapping.setCreatedDate(LocalDateTime.now());
+        urlMapping.setShortUrl(shortUrl);
+
+        Url_Mapping savedUrlMapping = repo.save(urlMapping);
+        return convertToDto(savedUrlMapping);
+    }
+
+    private UrlMappingDTO convertToDto(Url_Mapping urlMapping) {
+        UrlMappingDTO dto = new UrlMappingDTO();
+        dto.setOriginalUrl(urlMapping.getOriginalUrl());
+        dto.setUsername(urlMapping.getUsers().getUsername());
+        dto.setCreatedDate(urlMapping.getCreatedDate());
+        dto.setShortUrl(urlMapping.getShortUrl());
+        dto.setId(urlMapping.getId());
+        dto.setClickCount(urlMapping.getClickCount());
+        return dto;
+    }
+
+    private String generateShortUrl() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        StringBuilder shortUrl = new StringBuilder(8);
+
+        for (int i = 0; i < 8; i++) {
+            shortUrl.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        return shortUrl.toString();
+    }
+}
